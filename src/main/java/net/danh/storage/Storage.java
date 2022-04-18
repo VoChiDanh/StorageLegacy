8460a985d72d5fbe0e1dc4ccd08f80564c866e06
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-04-18 22:59:29 +0700

    Revert "Bump version"
    This reverts commit a9fd8b8dbbaf82fb2b9093aad474f09199ce49c7.

--- src/main/java/net/danh/storage/Storage.java
+++ src/main/java/net/danh/storage/Storage.java
@@ -118,10 +118,10 @@ public final class Storage extends PonderBukkitPlugin implements Listener {
     }
 
     private void checkFilesVersion() {
-        if (!Objects.requireNonNull(getconfigfile().getString("VERSION")).equalsIgnoreCase("1.0-B4") || getconfigfile().getString("VERSION") == null) {
+        if (!Objects.requireNonNull(getconfigfile().getString("VERSION")).equalsIgnoreCase(getDescription().getVersion()) || getconfigfile().getString("VERSION") == null) {
             getLogger().log(Level.SEVERE, "You need update config.yml!");
         }
-        if (!Objects.requireNonNull(getlanguagefile().getString("VERSION")).equalsIgnoreCase("1.0-B4") || getconfigfile().getString("VERSION") == null) {
+        if (!Objects.requireNonNull(getlanguagefile().getString("VERSION")).equalsIgnoreCase(getDescription().getVersion()) || getconfigfile().getString("VERSION") == null) {
             getLogger().log(Level.SEVERE, "You need update language.yml!");
         }
     }
