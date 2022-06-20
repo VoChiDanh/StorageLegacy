261055b1f537308b2943532f60389f4fd8973eac
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-06-20 13:39:30 +0700

    add `DECORATES_SLOTS` to check click slot.... Fix small bug Didn't have time to test it, i'll test after 6h

--- src/main/java/net/danh/storage/Gui/InventoryClick.java
+++ src/main/java/net/danh/storage/Gui/InventoryClick.java
@@ -13,6 +13,8 @@ import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.List;
+
 import static net.danh.dcore.Utils.Player.sendPlayerMessage;
 import static net.danh.storage.Manager.Files.*;
 
@@ -50,53 +52,57 @@ public class InventoryClick implements Listener {
                             }
                         }
                     }
-                    if (e.getClick() == ClickType.LEFT) {
-                        Data.click.put(p, ClickType.LEFT);
-                        Data.action.add(p);
-                        Data.item.put(p, name);
-                        p.closeInventory();
-                        sendPlayerMessage(p, getlanguagefile().getString("Input.Chat"));
-                    }
-                    if (e.getClick() == ClickType.RIGHT) {
-                        Data.click.put(p, ClickType.RIGHT);
-                        Data.action.add(p);
-                        Data.item.put(p, name);
-                        p.closeInventory();
-                        sendPlayerMessage(p, getlanguagefile().getString("Input.Chat"));
-                    }
-                    if (e.getClick() == ClickType.DROP) {
-                        Data.click.put(p, ClickType.DROP);
-                        Data.action.add(p);
-                        Data.item.put(p, name);
-                        p.closeInventory();
-                        sendPlayerMessage(p, getlanguagefile().getString("Input.Chat"));
-                    }
-                    if (e.getClick() == ClickType.SHIFT_LEFT) {
-                        String finalName = name;
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                p.performCommand("storage take " + finalName + " all");
-                            }
-                        }.runTask(Storage.get());
-                    }
-                    if (e.getClick() == ClickType.SHIFT_RIGHT) {
-                        String finalName1 = name;
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                p.performCommand("storage add " + finalName1 + " all");
-                            }
-                        }.runTask(Storage.get());
-                    }
-                    if (e.getClick() == ClickType.CONTROL_DROP) {
-                        String finalName2 = name;
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                p.performCommand("storage sell " + finalName2 + " all");
-                            }
-                        }.runTask(Storage.get());
+                    int c_s = e.getSlot();
+                    List<Integer> d_s = getguifile().getIntegerList("DECORATES_SLOTS");
+                    if (!d_s.contains(c_s)) {
+                        if (e.getClick() == ClickType.LEFT) {
+                            Data.click.put(p, ClickType.LEFT);
+                            Data.action.add(p);
+                            Data.item.put(p, name);
+                            p.closeInventory();
+                            sendPlayerMessage(p, getlanguagefile().getString("Input.Chat"));
+                        }
+                        if (e.getClick() == ClickType.RIGHT) {
+                            Data.click.put(p, ClickType.RIGHT);
+                            Data.action.add(p);
+                            Data.item.put(p, name);
+                            p.closeInventory();
+                            sendPlayerMessage(p, getlanguagefile().getString("Input.Chat"));
+                        }
+                        if (e.getClick() == ClickType.DROP) {
+                            Data.click.put(p, ClickType.DROP);
+                            Data.action.add(p);
+                            Data.item.put(p, name);
+                            p.closeInventory();
+                            sendPlayerMessage(p, getlanguagefile().getString("Input.Chat"));
+                        }
+                        if (e.getClick() == ClickType.SHIFT_LEFT) {
+                            String finalName = name;
+                            new BukkitRunnable() {
+                                @Override
+                                public void run() {
+                                    p.performCommand("storage take " + finalName + " all");
+                                }
+                            }.runTask(Storage.get());
+                        }
+                        if (e.getClick() == ClickType.SHIFT_RIGHT) {
+                            String finalName1 = name;
+                            new BukkitRunnable() {
+                                @Override
+                                public void run() {
+                                    p.performCommand("storage add " + finalName1 + " all");
+                                }
+                            }.runTask(Storage.get());
+                        }
+                        if (e.getClick() == ClickType.CONTROL_DROP) {
+                            String finalName2 = name;
+                            new BukkitRunnable() {
+                                @Override
+                                public void run() {
+                                    p.performCommand("storage sell " + finalName2 + " all");
+                                }
+                            }.runTask(Storage.get());
+                        }
                     }
                 }
             }
